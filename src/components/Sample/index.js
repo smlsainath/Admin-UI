63563814be776e81e860a3a582fa20d91d765bba
smlsainath <smlsainath@gmail.com>
2021-07-18 18:10:08 +0530

    Update index.js

--- src/components/Sample/index.js
+++ src/components/Sample/index.js
@@ -7,68 +7,32 @@ import AdminItem from '../AdminItem';
 
 export default class Sample extends Component {
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            offset: 0,
-            data: [],
-            perPage: 10,
-            currentPage: 0
-        };
-        this.handlePageClick = this
-            .handlePageClick
-            .bind(this);
-}
+    
+    state = {
+        offset: 0,
+        perPage: 10,
+        currentPage: 0
+    };
+        
 
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const {perPage} = this.state
+        const offset = selectedPage * perPage;
 
-componentDidMount() {
-        this.receivedData()
+        this.setState({
+             offset
+        });
+
     }
-
-handlePageClick = (e) => {
-    const selectedPage = e.selected;
-    const {perPage} = this.state
-    const offset = selectedPage * perPage;
-
-    this.setState({
-        currentPage: selectedPage,
-         offset
-    }, () => {
-        this.receivedData()
-    });
-
-};
-
-receivedData(){
-    const{adminData} = this.props 
-    
-   
-    
-    // axios
-    //  .get(apiUrl)
-    //     .then(res => {
-
-                const data = adminData;
-               
-
-                const {offset,perPage} = this.state
-                const slice = data.slice(offset, offset + perPage)
-                const postData = slice.map(pd => <>
-                    <AdminItem key={pd.id} admin={pd} />
-                </>)
-
-                this.setState({
-                    pageCount: Math.ceil(data.length / perPage),
-
-                    postData
-                })
-            
-    }
-    
-
     
     render() {
-        const{postData, pageCount} = this.state
+        const{ offset,perPage} = this.state
+        const{adminData} = this.props 
+        const slice = adminData.slice(offset, offset + perPage)
+        const postData = slice.map(pd => <>
+            <AdminItem key={pd.id} admin={pd} />
+        </>)
         return (
             <div>
                 {postData}
@@ -77,7 +41,7 @@ receivedData(){
                     nextLabel="next"
                     breakLabel="..."
                     breakClassName="break-me"
-                    pageCount={pageCount}
+                    pageCount={Math.ceil(adminData.length / perPage)}
                     marginPagesDisplayed={2}
                     pageRangeDisplayed={5}
                     onPageChange={this.handlePageClick}
